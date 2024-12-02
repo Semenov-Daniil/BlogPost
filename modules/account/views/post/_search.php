c5e857f07fbcba3c60a70b155a88c28b7831eb90
Semenov-Daniil <ds.daniilsemen.ds@gmail.com>
2024-12-02 14:46:06 +0300

    create search form

---
+++ modules/account/views/post/_search.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\modules\account\models\PostsSearch $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="posts-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+        'options' => [
+            'data-pjax' => 1
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'title') ?>
+
+    <?= $form->field($model, 'preview') ?>
+
+    <?= $form->field($model, 'text') ?>
+
+    <?= $form->field($model, 'users_id') ?>
+
+    <?php // echo $form->field($model, 'themes_id') ?>
+
+    <?php // echo $form->field($model, 'statuses_id') ?>
+
+    <?php // echo $form->field($model, 'created_at') ?>
+
+    <?php // echo $form->field($model, 'updated_at') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
