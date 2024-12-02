f423b33f7e34543c2e0a2313a4e9cb634ccf80eb
Semenov-Daniil <ds.daniilsemen.ds@gmail.com>
2024-12-02 14:46:43 +0300

    create new migration

---
+++ migrations/m241202_074902_insert_rows_in_statuses_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m241202_074902_insert_rows_in_statuses_table
+ */
+class m241202_074902_insert_rows_in_statuses_table extends Migration
+{
+    const TABLE_NAME = '{{%roles}}';
+    const ADMIN = 'admin';
+    const AUTHOR = 'author';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->batchInsert(self::TABLE_NAME, ['title'], 
+        [
+            [
+                'Редактирование'
+            ],
+            [
+                'Одобрен'
+            ]
+            [
+                'Запрещен'
+            ],
+            [
+                'На модерации'
+            ]
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->delete(self::TABLE_NAME, ['title' => 'Редактирование']);
+        $this->delete(self::TABLE_NAME, ['title' => 'Одобрен']);
+        $this->delete(self::TABLE_NAME, ['title' => 'Запрещен']);
+        $this->delete(self::TABLE_NAME, ['title' => 'На модерации']);
+    }
+}
